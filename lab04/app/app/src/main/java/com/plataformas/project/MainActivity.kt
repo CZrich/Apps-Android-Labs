d706285d13a25b98d212a71a8cef8eae2d421a09
CZrich <czrichardz@gmail.com>
2025-10-11 15:04:29 -0500

    lab04

---
+++ lab04/app/app/src/main/java/com/plataformas/project/MainActivity.kt
@@ -0,0 +1,59 @@
+package com.plataformas.project
+
+import android.app.FragmentManager
+import androidx.fragment.app.Fragment
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.FragmentActivity
+
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.plataformas.project.frames.ComunidadFragment
+import com.plataformas.project.frames.HomeFragment
+import com.plataformas.project.frames.MapFragment
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var  bottomNavigation: BottomNavigationView
+    //private lateinit var  fragmentTransation:
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        bottomNavigation = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+
+        //por defaul estar en home
+
+        replaceFragment(HomeFragment())
+
+        //cabia los framges
+        bottomNavigation.setOnItemSelectedListener {
+
+            item ->
+            when(item.itemId){
+
+                R.id.menu_home -> replaceFragment(HomeFragment())
+                R.id.menu_comunidad->replaceFragment(ComunidadFragment())
+                R.id.menu_mapa-> replaceFragment(MapFragment())
+            }
+            true
+        }
+
+    }
+
+
+    // funcion para cambiar de fragments
+    private fun replaceFragment(fragment: Fragment){
+      val transaction = supportFragmentManager.beginTransaction()
+      transaction.setCustomAnimations(android.R.anim.fade_in,
+          android.R.anim.fade_out
+          )
+        transaction.replace(R.id.fragmentContainerView2,fragment)
+        transaction.commit()
+
+    }
+
+
+
+
+}
